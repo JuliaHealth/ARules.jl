7d59bea781c0e1d1003c6fbc9d3f8bc1f99cb3b9
paulstey <paulstey@gmail.com>
2017-08-11 15:34:45 -0400

    fixed order of export and include()

--- src/ARules.jl
+++ src/ARules.jl
@@ -3,14 +3,16 @@ module ARules
 using StatsBase 
 using DataTables 
 
-include("frequent_itemset_tree.jl")
-include("utils.jl")
-include("rule_generation.jl")
-
 export Node, Rule, apriori, frequent, get_unique_items, shownodes, 
        # these below are for pilotting
        has_children, younger_siblings, growtree!, rands, occurence, gen_support_dict,
        gen_rules!, gen_rules, gen_node_rules!
+
+
+include("frequent_itemset_tree.jl")
+include("utils.jl")
+include("rule_generation.jl")
+
 
 # package code goes here
 
