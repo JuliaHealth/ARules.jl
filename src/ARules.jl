4dd9e458aba37b7a0dddfc21ce62747a7e3da23c
paulstey <paulstey@gmail.com>
2017-08-11 15:27:13 -0400

    added a ton of exports

--- src/ARules.jl
+++ src/ARules.jl
@@ -1,16 +1,17 @@
 module ARules
 
+using StatsBase 
+using DataTables 
+
 include("frequent_itemset_tree.jl")
 include("rules.jl")
 include("utils.jl")
 include("rule_generation.jl")
 
-export Node, 
-       Rule, 
-       apriori, 
-       frequent,
-       get_unique_items,
-       shownodes
+export Node, Rule, apriori, frequent, get_unique_items, shownodes, 
+       # these below are for pilotting
+       has_children, younger_siblings, growtree!, rands, occurence, gen_support_dict,
+       gen_rules!, gen_rules, gen_node_rules!
 
 # package code goes here
 
