144d25dd945b565c04f388d31a68eb3d6d43b2b3
paulstey <paulstey@gmail.com>
2017-08-11 14:27:37 -0400

    adding some includes

--- src/ARules.jl
+++ src/ARules.jl
@@ -1,5 +1,16 @@
 module ARules
 
+include("frequent_itemset_tree.jl")
+include("rules.jl")
+include("utils.jl")
+
+export Node, 
+       Rule, 
+       apriori, 
+       frequent,
+       get_unique_items,
+       shownodes
+
 # package code goes here
 
 end # module
