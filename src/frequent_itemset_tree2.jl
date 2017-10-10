0744a4142c07bd407265822502b3471b527e2745
paulstey <paulstey@gmail.com>
2017-10-10 14:29:42 -0400

    print pre-map address and post-map address

--- src/frequent_itemset_tree2.jl
+++ src/frequent_itemset_tree2.jl
@@ -55,8 +55,9 @@ function growtree!(nd::Node, minsupp, k, maxdepth, node_idx, node_arr)
             n_nodes += 1
             push!(node_arr, child)          # add child node to master node array
             push!(nd.children, n_nodes)     # n_nodes is the child's node index
+            println("pre-map address ", pointer_from_objref(node_arr[n_nodes].transactions))
             node_arr[n_nodes].transactions = map(x -> false, node_arr[n_nodes].transactions)
-            println("outer sum: ", sum(transacts), " inner sum: ", sum(node_arr[n_nodes].transactions))
+            println("post-map address ", pointer_from_objref(node_arr[n_nodes].transactions))
         end
     end
     # Recurse on newly created children
