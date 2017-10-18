be8c5f91f23978cbb5502bd10ac4b6a53c852544
paulstey <paulstey@gmail.com>
2017-10-18 16:17:31 -0400

    minor updates

--- test/benchmarking.jl
+++ test/benchmarking.jl
@@ -3,16 +3,20 @@ using StatsBase
 using BenchmarkTools
 
 
-itemlist = randstr(100);
+const itemlist = randstr(100);
 
-n = 100_000
-m = 25             # number of items in transactions
-mx_depth = 7
-t = [sample(itemlist, m, replace = false) for _ in 1:n];
+const n = 100_000
+const m = 25             # number of items in transactions
+const mx_depth = 7
+const t = [sample(itemlist, m, replace = false) for _ in 1:n];
 
 # @code_warntype _frequent(t, 1)
-@time unq2 = unique_items(t)
+const unq2 = unique_items(t)
 # @benchmark occ2 = occurrence(t, unq2)
-@time itree2 = frequent_item_tree(t, unq2, round(Int, 0.01*n), mx_depth)
+itree2 = frequent_item_tree(t, unq2, round(Int, 0.01*n), mx_depth)
 
-ARules.shownodes(itree2)
+Profile.clear_malloc_data()
+itree2 = frequent_item_tree(t, unq2, round(Int, 0.01*n), mx_depth)
+
+
+# ARules.shownodes(itree2)
